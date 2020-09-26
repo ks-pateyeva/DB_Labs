b9b38744c9b932a143e51e0e693460d827a5e1ea
Unknown <ks.pateeva@gmail.com>
2020-09-26 18:53:04 +0300

    Add sql script for the 2nd task

---
+++ Lab1/V2_2.sql
@@ -0,0 +1,45 @@
+USE [AdventureWorks2012];
+GO
+
+---- ВАРИАНТ 2 ----
+--Вывести на экран список отделов, отсортированных по названию отдела в порядке Z-A.
+--Вывести на экран только 5 строк, начиная с 3-ей строки.
+
+SELECT
+	[DepartmentID],
+	[Name]
+FROM [HumanResources].[Department]
+
+ORDER BY [Name] DESC
+OFFSET 2 ROWS
+FETCH NEXT 5 ROWS ONLY;
+
+GO
+
+-- Вывести на экран список неповторяющихся позиций, которые
+-- соответствуют первому уровню позиций в организации (OrganizationLevel).
+
+SELECT DISTINCT
+	[JobTitle]
+FROM [HumanResources].[Employee]
+
+WHERE
+	[OrganizationLevel] = 1;
+
+GO
+
+-- Вывести на экран сотрудников, которым исполнилось 18 лет
+-- в тот год, когда их приняли на работу.
+
+SELECT
+	[BusinessEntityID],
+	[JobTitle],
+	[Gender],
+	[BirthDate],
+	[HireDate]
+FROM [HumanResources].[Employee]
+
+WHERE 
+	YEAR([HireDate]) - YEAR([BirthDate]) = 18;
+
+GO
