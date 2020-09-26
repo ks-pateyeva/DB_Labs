5e5da4ed9c7b526f76ed5a1cbd050fd81e537cf2
Unknown <ks.pateeva@gmail.com>
2020-09-26 18:45:50 +0300

    Lab1

---
+++ Lab1/V2_1.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE NewDatabase;
+GO
+
+USE NewDatabase;
+GO
+
+CREATE SCHEMA sales;
+GO
+
+CREATE SCHEMA persons;
+GO
+
+CREATE TABLE sales.Orders (OrderNum INT NULL);
+GO
+
+BACKUP DATABASE NewDatabase TO DISK = 'd:\BSUIR\BD\KSENIYA_PATEYEVA.bak';
+GO
+
+USE master;
+GO
+
+DROP DATABASE NewDatabase;
+GO
+
+RESTORE DATABASE NewDatabase FROM DISK = 'd:\BSUIR\BD\KSENIYA_PATEYEVA.bak';
+GO
